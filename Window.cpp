e499a19598effd3a3defbf9ed292e471eb90d62e
xCharlesxx <Charlie2.saunders@live.uwe.ac.uk>
2017-11-18 13:21:27 +0000

    Update

--- Window.cpp
+++ Window.cpp
@@ -30,22 +30,22 @@ LRESULT CALLBACK Window::MessageHandler(HWND hwnd, UINT umsg, WPARAM wparam, LPA
 {
 	switch (umsg)
 	{
-			// Check if a key has been pressed on the keyboard.
+			//Check if a key has been pressed on the keyboard.
 		case WM_KEYDOWN:
 		{
-			// If a key is pressed send it to the input object so it can record that state.
+			//If a key is pressed send it to the input object so it can record that state.
 			m_input.KeyDown((unsigned int)wparam);
 			return 0;
 		}
 
-		// Check if a key has been released on the keyboard.
+		//Check if a key has been released on the keyboard.
 		case WM_KEYUP:
 		{
-			// If a key is released then send it to the input object so it can unset the state for that key.
+			//If a key is released then send it to the input object so it can unset the state for that key.
 			m_input.KeyUp((unsigned int)wparam);
 			return 0;
 		}
-		// Any other messages send to the default message handler as our application won't make use of them.
+		//Any other messages send to the default message handler as our application won't make use of them.
 		default:
 		{
 			return DefWindowProc(hwnd, umsg, wparam, lparam);
